f8a7ae90398c542e2901650948d936bccd8c89ef
edgarbarney <edwardgmgmd@gmail.com>
2022-01-02 16:12:53 +0300

    Personalize Somethings
    Don't worry. I'll add another credits thingy.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,13 +2,13 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("A Discord Rich Presence extension for Visual Studio 2017, 2019, and 2022")]
-[assembly: AssemblyDescription("A Discord Rich Presence extension for Visual Studio 2017, 2019, and 2022")]
+[assembly: AssemblyTitle("A Discord Rich Presence extension for Visual Studio 2022")]
+[assembly: AssemblyDescription("A Discord Rich Presence extension for Visual Studio 2022")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("1TheNikita")]
+[assembly: AssemblyCompany("FranticDreamer")]
 [assembly: AssemblyProduct("Discord Rich Presence VS extension")]
-[assembly: AssemblyCopyright("Copyright 1TheNikita 2021")]
-[assembly: AssemblyTrademark("1TheNikita")]
+[assembly: AssemblyCopyright("Copyright FranticDreamer 2022")]
+[assembly: AssemblyTrademark("FranticDreamer")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("6.0.0")]
